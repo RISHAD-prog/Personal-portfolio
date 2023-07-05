513df60ff99fbcfd17a1dbbd69980eac98e749a3
RISHAD-prog <20-43002-1@student.aiub.edu>
2023-07-05 16:45:54 +0600

    version-05 portfolio

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,9 +1,31 @@
-
+import { useState } from "react";
+import emailjs from 'emailjs-com';
 const Contact = () => {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        console.log(name, email, message);
+
+        emailjs.sendForm('service_kdrenc8', 'template_jfv3mqc', e.target, 'b7I_Sq_Cz5GuAwkPS')
+            .then((response) => {
+                console.log('Email sent successfully:', response.text);
+                // Reset the form
+                setName('');
+                setEmail('');
+                setMessage('');
+            })
+            .catch((error) => {
+                console.error('Error sending email:', error);
+            });
+    };
     return (
         <div id="contact">
-            <h1 className="text-4xl font-bold text-warning mt-16 mb-3 text-center">Contact With Me!</h1>
-            <div className="hero min-h-screen bg-transparent my-16">
+            <h1 className="text-4xl font-bold text-warning mt-60  text-center">Contact With Me!</h1>
+            <div className="hero min-h-screen bg-transparent mb-16">
                 <div className="hero-content flex-col lg:flex-row-reverse">
                     <div className="text-center lg:text-left">
 
@@ -11,31 +33,31 @@ const Contact = () => {
                     </div>
                     <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
                         <h1 className="text-3xl text-center font-bold">Send Email !</h1>
-                        <div className="card-body">
+                        <form className="card-body" onSubmit={handleSubmit}>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Name</span>
                                 </label>
-                                <input type="text" placeholder="Your Name" className="input input-bordered" />
+                                <input type="text" name="from_name" value={name} placeholder="Your Name" className="input input-bordered" onChange={(e) => setName(e.target.value)} />
                             </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Email</span>
                                 </label>
-                                <input type="email" placeholder="Your Email" className="input input-bordered" />
+                                <input type="email" name="from_email" value={email} placeholder="Your Email" className="input input-bordered" onChange={(e) => setEmail(e.target.value)} />
 
                             </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Message</span>
                                 </label>
-                                <textarea className="textarea textarea-bordered h-24" placeholder="Message"></textarea>
+                                <textarea value={message} name="message" className="textarea textarea-bordered h-24" placeholder="Message" onChange={(e) => setMessage(e.target.value)}></textarea>
 
                             </div>
                             <div className="form-control mt-6">
                                 <button className="btn btn-primary">Send </button>
                             </div>
-                        </div>
+                        </form>
                     </div>
                 </div>
             </div>
--- src/components/Education/Education.jsx
+++ src/components/Education/Education.jsx
@@ -2,9 +2,9 @@ import { Link } from "react-router-dom";
 
 const Education = () => {
     return (
-        <div id="education" className="my-10">
-            <h1 className="text-center text-3xl font-bold text-warning">Education</h1>
-            <div className="flex items-center gap-24 px-72 py-11">
+        <div id="education" className="my-16">
+            <h1 className="text-center text-3xl font-bold text-warning mb-4">Education</h1>
+            <div className="flex items-center gap-24 mx-60 p-11 shadow-2xl">
                 <div className=" ">
                     <Link to="https://www.aiub.edu/" className="flex items-center text-2xl gap-8 font-serif font-bold text-blue-700"><img
                         className="w-36" src="https://www.aiub.edu/Files/Templates/NewAIUB/assets/images/aiub-logo.svg" alt="American International University-Bangladesh" />American <br /> International <br /> University- <br />Bangladesh</Link></div>
